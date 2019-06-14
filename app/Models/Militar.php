58b48dab8fc874806ac0cf77ce3e8f7d9f600867
Litiano Moura <litiano.moura@yahoo.com.br>
2019-06-14 17:45:59 -0300

    Adicionado método com muitos argumentos

--- app/Models/Militar.php
+++ app/Models/Militar.php
@@ -18,4 +18,34 @@ class Militar extends Model
     {
         return true;
     }
+
+    public function funcaoComplexa($teste)
+    {
+        switch ($teste) {
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            case 3:
+                return 3;
+            case 4:
+                return 4;
+            case 6:
+                return 6;
+            case 7:
+                return 7;
+            case 8:
+                return 8;
+            case 9:
+                return 9;
+            default:
+                return 0;
+        }
+    }
+
+    public function Nome_Estranho() {
+        return null;
+    }
+
+    public function naoIndentada() {return 2+2;}
 }
